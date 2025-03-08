66f599eb46cb57b1fc2db1823a2921997098f2b2
Vitor Leal <vitorlealeng@gmail.com>
2025-03-08 19:52:49 -0300

    feat: add endpoint to create user

---
+++ src/infrastructure/web/user_controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { UserService } from "../../application/services/user_service";
+import { CreateUserDTO } from "../../application/dtos/create_user_dto";
+
+export class UserController {
+  constructor(private readonly userService: UserService) {
+  }
+
+  async createUser(req: Request, res: Response): Promise<Response> {
+    try {
+      const name = req.body.name;
+      const userDto: CreateUserDTO = { name };
+      const user = await this.userService.createUser(userDto);
+
+      return res.status(201).json({
+        message: "Usuário criado com sucesso",
+        data: {
+          id: user.getId(),
+          name: user.getName(),
+        },
+      });
+    } catch (error: any) {
+      return res
+        .status(400)
+        .json({ message: error.message || "An unexpected error occurred" });
+    }
+  }
+}
---
+++ src/infrastructure/web/user_controller_e2e.test.ts
@@ -0,0 +1,60 @@
+import express from "express";
+import request from "supertest";
+import { DataSource } from "typeorm";
+import { TypeORMUserRepository } from "../repositories/typeorm_user_repository";
+import { UserService } from "../../application/services/user_service";
+import { UserEntity } from "../persistence/entities/user_entity";
+import { UserController } from "./user_controller";
+
+const app = express();
+app.use(express.json());
+
+let dataSource: DataSource;
+let userRepository: TypeORMUserRepository;
+let userService: UserService;
+let userController: UserController;
+
+beforeAll(async () => {
+  dataSource = new DataSource({
+    type: "sqlite",
+    database: ":memory:",
+    dropSchema: true,
+    entities: [UserEntity],
+    synchronize: true,
+    logging: false,
+  });
+
+  await dataSource.initialize();
+
+  userRepository = new TypeORMUserRepository(
+    dataSource.getRepository(UserEntity)
+  );
+  userService = new UserService(userRepository);
+  userController = new UserController(userService);
+
+  app.post("/users", (req, res, next) => {
+    userController.createUser(req, res).catch((err) => next(err));
+  });
+});
+
+afterAll(async () => {
+  await new Promise(resolve => setTimeout(resolve, 500));
+  await dataSource.destroy();
+});
+
+describe("UserController", () => {
+  beforeEach(async () => {
+    const userRepo = dataSource.getRepository(UserEntity);
+    await userRepo.clear();
+  });
+
+  it("deve criar um usuário com sucesso", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Vitor Teste",
+    });
+    expect(response.status).toBe(201);
+    expect(response.body.message).toBe("Usuário criado com sucesso");
+    expect(response.body.data).toHaveProperty("id");
+    expect(response.body.data).toHaveProperty("name");
+  });
+});
